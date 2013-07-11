e0bd40dd73cd7ef7e953d710416057c5c7b010b8
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2013-07-11 23:36:32 +0000

    read index.html from disk

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexStr = fs.readFileSync("index.html", "utf-8");
+  response.send(indexStr);
 });
 
 var port = process.env.PORT || 5000;
